850e0ae07837fda8a2cd8cbe465c4bd5bb04d419
Aye Aye Myat <ayeayemyat.metro@gmail.com>
2026-05-14 15:36:49 +0630

    Update server.js

--- server.js
+++ server.js
@@ -1,42 +1,50 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 const path = require('path');
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+let callHistory = [];
+let players = {};
+let isGameOver = false;
 
 app.use(express.static(__dirname));
 
-let players = [];
-
 io.on('connection', (socket) => {
-    console.log('User connected:', socket.id);
+    socket.emit('gameUpdate', { history: callHistory });
+    io.emit('updatePlayerCount', Object.keys(players).length);
 
-    // Player အသစ်ဝင်လာခြင်း
-    socket.on('joinGame', (username) => {
-        players.push({ id: socket.id, name: username });
-        io.emit('updatePlayerCount', players.length);
-    });
-
-    // နံပါတ်နှိပ်လိုက်ချိန် (Draw Number)
-    socket.on('drawNumber', () => {
-        const number = Math.floor(Math.random() * 99) + 1;
-        io.emit('numberDrawn', number);
-    });
-
-    // တစ်ယောက်ယောက် Bingo ဖြစ်သည့်အခါ
-    socket.on('playerWin', (name) => {
-        io.emit('announceWinner', name);
+    socket.on('registerPlayer', (name) => {
+        players[socket.id] = name;
+        io.emit('updatePlayerCount', Object.keys(players).length);
     });
 
     socket.on('disconnect', () => {
-        players = players.filter(p => p.id !== socket.id);
-        io.emit('updatePlayerCount', players.length);
+        if (players[socket.id]) {
+            delete players[socket.id];
+            io.emit('updatePlayerCount', Object.keys(players).length);
+        }
+    });
+
+    socket.on('callNumber', () => {
+        if (isGameOver || callHistory.length >= 75) return;
+        let num;
+        do { num = Math.floor(Math.random() * 75) + 1; } while (callHistory.includes(num));
+        callHistory.push(num);
+        io.emit('gameUpdate', { history: callHistory, calledAt: Date.now() });
+    });
+
+    socket.on('playerWin', (data) => {
+        isGameOver = true;
+        io.emit('announceWinner', { winnerName: data.name });
+    });
+
+    socket.on('resetGame', () => {
+        callHistory = [];
+        isGameOver = false;
+        io.emit('gameUpdate', { history: [], calledAt: null });
     });
 });
 
-server.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-});
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, '0.0.0.0', () => console.log(`Server is running on port ${PORT}!`));
